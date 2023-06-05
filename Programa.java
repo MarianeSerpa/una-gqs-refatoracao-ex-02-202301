62fc11bb26f3dbc1fb21bc5ac7409a88b4ec05fa
Mariane <mariane@gmail.com>
2023-06-05 15:45:23 -0300

    feat: Adicionado condição que valida a idade das mulheres menor ou igual a 50 anos com mensalidade de 65

--- Programa.java
+++ Programa.java
@@ -33,7 +33,9 @@ public class Programa {
         mensalidade = 90.0;
       } else if (idade <= 40) {
         mensalidade = 85.0;
-      } else {
+      } else if (idade <= 50) {
+        mensalidade = 65.0;
+      }else {
         mensalidade = 80.0;
       }
     } else {
